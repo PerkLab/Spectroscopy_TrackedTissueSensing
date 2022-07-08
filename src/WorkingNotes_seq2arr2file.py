7e86a3dec77733460904d963e45feef260400c07
16djm10 <16djm10@queensu.ca>
2022-07-08 15:21:32 -0400

    re #6: Added the ability to save the spectrums to a csv

--- src/WorkingNotes_seq2arr2file.py
+++ src/WorkingNotes_seq2arr2file.py
@@ -3,22 +3,17 @@ import numpy as np
 # pip.main(['install','pandas'])
 import pandas as pd
 
-
+folder = '../new_data/'
+# Select the label and append it to the end of the 
+label = 0
+name = 'cork'
 # This code is used to access a sequence from slicer and convert it to a numpy array to later export
 seqNode  = slicer.mrmlScene.GetFirstNodeByName('Sequence')
-# The sequence contains n data nodes, in this first case 44 of them
-arr_list = []
-for idx in range(seqNode.GetNumberOfDataNodes()): # 0 to 43
+# The sequence contains n data nodes
+folder = 'C:/OpticalSpectroscopy_TissueClassification/new_data/testFolder/'
+for idx in range(seqNode.GetNumberOfDataNodes()):
     volumeNode = seqNode.GetNthDataNode(idx)
     specArray = slicer.util.arrayFromVolume(volumeNode)
     specArray = np.squeeze(specArray)
     specArray = np.transpose(specArray)
-    arr_list.append(specArray)
-# convert the list to array format    
-output = np.array(arr_list)
-#add a column for the label (0 for table 1 for cork)
-
-#save the array to the file
-#fileName = 'testFile'
-#pd.DataFrame(output).to_csv(fileName + '.csv')
-#np.save(fileName, output)
+    np.savetxt(folder + name + str(idx) + '.csv', specArray, delimiter=',')
